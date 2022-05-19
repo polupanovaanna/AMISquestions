44c45b51b778154fc8d83d5551d71e7cfc6d683e
Maxim <max_mail2002@bk.ru>
2022-05-19 20:58:18 +0300

    checkbox is sorted by date

--- app/src/main/java/ru/fmcs/hse/amisquestions/NewPostsFragment.java
+++ app/src/main/java/ru/fmcs/hse/amisquestions/NewPostsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +20,7 @@ public class NewPostsFragment extends Fragment {
     private FragmentNewPostsBinding mBinding;
     private RecyclerView list;
     private PostPreviewAdapter adapter;
+    private CheckBox sbd;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,9 +35,13 @@ public class NewPostsFragment extends Fragment {
         list = view.findViewById(R.id.postList);
         LinearLayoutManager manager = new LinearLayoutManager(view.getContext());
         list.setLayoutManager(manager);
+        sbd = view.findViewById(R.id.sort_by_date);
 
         list.setHasFixedSize(false);
-        adapter = new PostPreviewAdapter();
+        sbd.setOnClickListener(view1 -> {
+           adapter.sortChanged();
+        });
+        adapter = new PostPreviewAdapter(sbd);
         list.setAdapter(adapter);
 
         System.out.println("here: " + (list == null));
